7222523f29ee4b0502a8aa86a958c9c723bcccba
sadohu <sadohu26@yahoo.com>
2023-08-14 16:48:37 -0500

    Configuracion de servidor, capa businnes y controller

---
+++ src/main/java/com/cibertec/DAW1_CL3_SALAS_HUGO/business/impl/ProductoServiceImpl.java
@@ -0,0 +1,46 @@
+package com.cibertec.DAW1_CL3_SALAS_HUGO.business.impl;
+
+import java.util.List;
+import java.util.Optional;
+
+import org.springframework.stereotype.Service;
+
+import com.cibertec.DAW1_CL3_SALAS_HUGO.business.ProductoService;
+import com.cibertec.DAW1_CL3_SALAS_HUGO.model.Producto;
+import com.cibertec.DAW1_CL3_SALAS_HUGO.repository.ProductoRepository;
+
+import lombok.AllArgsConstructor;
+
+@Service
+@AllArgsConstructor
+public class ProductoServiceImpl implements ProductoService{
+
+	private ProductoRepository repository;
+
+	@Override
+	public Producto buscarProducto(int id) {
+		Optional<Producto> obj = repository.findById(id);
+		Producto producto = null;
+		if(obj.isPresent()) {
+			producto = obj.get();
+		}else {
+			throw new RuntimeException("ID incorrecto: Producto no fue encontrado.");
+		}
+		return producto;
+	}
+	
+	@Override
+	public List<Producto> listarProducto() {
+		return repository.findAll();
+	}
+
+	@Override
+	public void registrarProducto(Producto producto) {
+		repository.save(producto);	
+	}
+
+	@Override
+	public void eliminarProducto(int id) {
+		repository.deleteById(id);
+	}
+}
