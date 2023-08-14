1ea8a27b88f51962317b3fcd80c759210ef29416
sadohu <sadohu26@yahoo.com>
2023-08-14 16:24:04 -0500

    add ProductoModel

---
+++ src/test/java/com/cibertec/DAW1_CL3_SALAS_HUGO/Daw1Cl3SalasHugoApplicationTests.java
@@ -0,0 +1,13 @@
+package com.cibertec.DAW1_CL3_SALAS_HUGO;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class Daw1Cl3SalasHugoApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
